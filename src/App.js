00cdbd39a85a825cfee7964a0fd95f19fce6d9df
brice <prodakil@gmail.com>
2023-05-22 15:56:51 +0200

    ✨ beautiful navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <ul className="navbar">
+          <a href="/">
+            <li>Home</li>
+          </a>
+          <a href="/loin">
+            <li>Login</li>
+          </a>
+          <a href="/">
+            <li>Dashboard</li>
+          </a>
+        </ul>
       </header>
     </div>
   );
