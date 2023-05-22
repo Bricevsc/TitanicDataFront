0c37e6327ea944a912f88e69e310032ad7941170
brice <prodakil@gmail.com>
2023-05-22 16:58:55 +0200

    💄

--- src/App.js
+++ src/App.js
@@ -3,19 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <ul className="navbar">
-          <a href="/">
-            <li>Home</li>
-          </a>
-          <a href="/loin">
-            <li>Login</li>
-          </a>
-          <a href="/">
-            <li>Dashboard</li>
-          </a>
-        </ul>
-      </header>
+
     </div>
   );
 }
---
+++ src/components/navbar.jsx
@@ -0,0 +1,17 @@
+const Navbar = () => {
+    return(
+        <ul className="navbar">
+        <a href="/">
+          <li>Home</li>
+        </a>
+        <a href="/loin">
+          <li>Login</li>
+        </a>
+        <a href="/">
+          <li>Dashboard</li>
+        </a>
+      </ul>
+    )
+}
+
+export default Navbar
