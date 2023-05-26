48973d683966bc05104787c6ac387f6f16243180
brice <prodakil@gmail.com>
2023-05-26 14:22:47 +0200

    ✨ navigate

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate()
   const [isLogin, setIsLogin] = useState(localStorage.getItem("authToken"));
   const deleteToken = () => {
     localStorage.clear();
     setIsLogin(false);
+    navigate("/");
   };
   return (
     <ul className="navbar">
